b9b11629104591553b50c78beceeb0d6fbadd725
Duvan <duvanmorenocardona.dmc@gmail.com>
2021-01-10 10:20:39 -0500

    vars

---
+++ variable/variable.py
@@ -0,0 +1,33 @@
+""" Integers """
+numero = 3
+numero1 = 5 
+numero2 = 6
+
+""" Floats """
+float1 = 2.3
+
+""" Text """
+text1 = 'Text'
+
+""" Booleasn """
+boolean1 = False
+boolean1 = True
+
+""" Change Data """
+numberInt = int('4')
+strData = str(2.3)
+floatData = float('2.3')
+
+""" Logic operators """
+# and -->
+# or --> 
+# not --> change value to negative or positive
+#
+
+""" compare operators """
+# a  == b--> compare two vars
+# a  != b--> distict two vars
+# a  > b--> min two vars
+
+
+
