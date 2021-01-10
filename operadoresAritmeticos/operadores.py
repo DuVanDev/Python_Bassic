10eb49be384d96fa1ecb499112a0c8eb784436b7
Duvan <duvanmorenocardona.dmc@gmail.com>
2021-01-10 10:20:33 -0500

    Operators

---
+++ operadoresAritmeticos/operadores.py
@@ -0,0 +1,25 @@
+""" Sumea """
+print(5 +5 )
+
+""" Multiply """
+print(2 * 5 )
+
+""" Divider """
+print(2 / 5 )
+
+""" Divider result only int """
+print( 2 // 5 )
+
+""" Module """
+print ( 2 % 5 )
+
+""" Exponent """
+print( 2 ** 5 )
+
+""" Raiz  """
+import math 
+""" other form to import is 
+from math import sqrt
+"""
+
+math.sqrt(2)
